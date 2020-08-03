6b3f28447f6ab08263b5e0b9f489dbb0a69ad4bd
lihqi <455711093@qq.com>
2020-08-03 13:51:53 +0000

    Deploying to gh-pages from master d4db1691ce8eb39e86c7b057748bd08da6814ed3

---
+++ precache-manifest.51796802a37b500ea281453aaa125f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25eb1e038cddc38e60e01cfe1a6cd86a",
+    "url": "/Roll-the-dice/index.html"
+  },
+  {
+    "revision": "c980fda02f8c018c3a8a",
+    "url": "/Roll-the-dice/static/css/main.400e8455.chunk.css"
+  },
+  {
+    "revision": "badbc9914ac4d779a874",
+    "url": "/Roll-the-dice/static/js/2.8167cb57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Roll-the-dice/static/js/2.8167cb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c980fda02f8c018c3a8a",
+    "url": "/Roll-the-dice/static/js/main.530b3b7a.chunk.js"
+  },
+  {
+    "revision": "824b8322c8bba884c293",
+    "url": "/Roll-the-dice/static/js/runtime-main.2527d78b.js"
+  }
+]);
